6e490bb80a46c6e9d5942f937c16ab6df2790842
ornaras <ornaras.us@gmail.com>
2025-11-28 11:41:27 +0400

    Добавлен класс API-клиента OFD.ru

---
+++ Recify.Parsers.OfdRu/OfdRuClient.cs
@@ -0,0 +1,11 @@
+﻿namespace Recify.Parsers.OfdRu;
+
+/// <summary>API-клиент OFD.ru</summary>
+public class OfdRuClient: HttpClient
+{
+    /// <summary>Инициализирует новый экземпляр <see cref="OfdRuClient"/>.</summary>
+    public OfdRuClient()
+    {
+        BaseAddress = new Uri("https://ofd.ru/api/integration/v2/");
+    }
+}
