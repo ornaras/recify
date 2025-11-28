668f9bbe8c60f872f3ee35bf1e3c50952b040ece
ornaras <ornaras.us@gmail.com>
2025-11-28 11:39:19 +0400

    Реализована структура ответа OfdRu

---
+++ Recify.Parsers.OfdRu/OfdRuEnums.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Recify.Parsers.OfdRu;
+
+/// <summary>Состояние обработки запроса.</summary>
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum OfdRuResponseStatus
+{
+    /// <summary>Запрос обработан успешно.</summary>
+    Success,
+    /// <summary>Обработка запроса не удалась.</summary>
+    Failed
+}
---
+++ Recify.Parsers.OfdRu/OfdRuResponse.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Recify.Parsers.OfdRu
+{
+    /// <summary>Структура ответа OFD.ru</summary>
+    public struct OfdRuResponse<T>
+    {
+        /// <summary>Состояние обработки запроса.</summary>
+        public OfdRuResponseStatus Status { get; set; }
+        /// <summary>Перечисление данных.</summary>
+        public IEnumerable<T> Data { get; set; }
+        /// <summary>Перечисление ошибок.</summary>
+        public IEnumerable<string> Errors { get; set; }
+        /// <summary>Время, затраченное на обработку запроса.</summary>
+        public TimeSpan Elapsed { get; set; }
+
+        /// <summary>Возвращает true, если запрос успешно отработан, иначе false</summary>
+        [JsonIgnore]
+        public readonly bool Success => Status == OfdRuResponseStatus.Success;
+
+        /// <summary>Формирует экземпляр исключения</summary>
+        public readonly Exception ToException() =>
+            new($"Запрос выполнен с ошибками: {string.Join(", ", Errors)}");
+    }
+}
